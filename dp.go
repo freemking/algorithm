2a48879b805f0b88c7bb59c6f979d8b3a01f392e
luca <freemking@gmail.com>
2020-05-08 22:51:47 +0800

    新增算法

--- dp.go
+++ dp.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 )
 
-```
+/*
 第一步要明确两点，「状态」和「选择」
 第二步要明确 dp 数组的定义
 第三步，根据「选择」，思考状态转移的逻辑
 最后一步，把伪码翻译成代码，处理一些边界情况
-```
+*/
 
 func main() {
 	//最长增长子序列
 	maxUpLength()
 	//0-1背包问题
 	zoreToOnePackage()
+	//完全背包问题
+	fullPackage()
+	//正则表达式
+	fmt.Println(isMatch("", ".*a*b*"))
 }
 
 func maxUpLength() {
@@ -33,11 +37,47 @@ func maxUpLength() {
 }
 
 func zoreToOnePackage() {
-	N = 3, W = 4
-	wt = [2, 1, 3]
-	val = [4, 2, 3]
-	
-	dp[n][w] = max(dp[n-1][w],dp[n-1][w-1])
+
+	n := 3
+	w := 4
+	wt := []int{2, 1, 3}
+	val := []int{4, 2, 3}
+
+	dp := make([][]int, n+1)
+
+	dp[0] = append(dp[0], 0, 0, 0, 0, 0)
+	for i := 1; i <= n; i++ {
+		dp[i] = append(dp[i], 0, 0, 0, 0, 0)
+		for j := 1; j <= w; j++ {
+			if j > wt[i-1] {
+				dp[i][j] = max(dp[i-1][j], dp[i-1][j-wt[i-1]]+val[i-1])
+			} else {
+				dp[i][j] = dp[i][j-1]
+			}
+		}
+	}
+	fmt.Println(dp[n][w])
+}
+
+func fullPackage() {
+
+}
+
+func isMatch(text, pattern string) bool {
+	if text == pattern {
+		return true
+	}
+	if pattern == "" {
+		return false
+	}
+
+	matchStatus := text != "" && (pattern[0] == '.' || text[0] == pattern[0])
+
+	if len(pattern) > 1 && pattern[1] == '*' {
+		return (matchStatus && isMatch(text[1:], pattern)) || isMatch(text, pattern[2:])
+	}
+
+	return matchStatus && isMatch(text[1:], pattern[1:])
 }
 
 func max(a, b int) int {
